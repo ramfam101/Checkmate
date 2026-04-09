3590578e7542f9ca41a08e4ee2499dc16eefd0de
guille <g.devita@ufl.edu>
2026-04-08 21:13:11 -0400

    Implemented Escalated Notifications

--- server/src/service/infrastructure/notificationsService.ts
+++ server/src/service/infrastructure/notificationsService.ts
@@ -14,6 +14,7 @@ export interface INotificationsService {
 	updateById(id: string, teamId: string, updateData: Partial<Notification>): Promise<Notification>;
 	deleteById: (id: string, teamId: string) => Promise<Notification>;
 	handleNotifications: (monitor: Monitor, monitorStatusResponse: MonitorStatusResponse, decision: MonitorActionDecision) => Promise<boolean>;
+	sendEscalatedNotifications: (notificationIds: string[], monitor: Monitor, monitorStatusResponse: MonitorStatusResponse, decision: MonitorActionDecision) => Promise<boolean>;
 
 	sendTestNotification: (notification: Partial<Notification>) => Promise<boolean>;
 	testAllNotifications: (notificationIds: string[]) => Promise<boolean>;
@@ -139,6 +140,20 @@ export class NotificationsService implements INotificationsService {
 
 		// Send notifications based on decision
 		return await this.sendNotifications(monitor, monitorStatusResponse, decision);
+	};
+
+	sendEscalatedNotifications = async (notificationIds: string[], monitor: Monitor, monitorStatusResponse: MonitorStatusResponse, decision: MonitorActionDecision): Promise<boolean> => {
+    	if (!notificationIds || notificationIds.length === 0) return false;
+
+    	const notifications = await this.notificationsRepository.findNotificationsByIds(notificationIds);
+    	const settings = this.settingsService.getSettings();
+    	const clientHost = settings.clientHost || "Host not defined";
+    	const notificationMessage = this.notificationMessageBuilder.buildMessage(monitor, monitorStatusResponse, decision, clientHost);
+
+    	const tasks = notifications.map((notification) => this.send(notification, monitor, monitorStatusResponse, decision, notificationMessage));
+    	const outcomes = await Promise.all(tasks);
+    	const succeeded = outcomes.filter(Boolean).length;
+    	return succeeded === notifications.length;
 	};
 
 	sendTestNotification = async (notification: Partial<Notification>) => {
