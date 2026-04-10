58a910abae2aad400faee6c46517f5764db22d63
MRuiz <mruiz92@ufl.edu>
2026-04-10 01:12:17 -0400

    Bug Fix 2

--- client/src/Validation/monitor.ts
+++ client/src/Validation/monitor.ts
@@ -15,8 +15,8 @@ const baseSchema = z.object({
 	notifications: z.array(z.string()),
 	escalateAfter: z.number().min(0, "Must be 0 or greater"),
     escalationChannels: z.array(z.string()),
-	lastStatusChange: z.union([z.date(), z.string()]), // Add this!
-    isEscalated: z.boolean().default(false),
+	lastStatusChange: z.union([z.date(), z.string()]).optional(), // Add this!
+    isEscalated: z.boolean().optional(),
 	statusWindowSize: z
 		.number({ message: "Status window size is required" })
 		.min(1, "Status window size must be at least 1")
